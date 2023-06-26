f73a21b90f03c62abe859e406c019b5ffda34dfa
Mateo Osorio <118546753+MateoOsorio016@users.noreply.github.com>
2023-06-25 21:44:53 -0500

    Update pedido.js
    Proveedor, Producto y cantidad

--- models/pedido.js
+++ models/pedido.js
@@ -6,6 +6,22 @@ const PedidoSchema=Schema({
         unique:true,
         required:[true, "el nit es obligatorio"]
     },
+    Proveedor:{
+        type:String,
+        required:[true, "el proveedor es obligatorio"]
+    },
+    Producto:{
+        type:String,
+        required:[true, "el prodcuto es obligatorio"]
+    },
+    Cantidad:{
+        type:Number,
+        required:[true, "la cantidad es obligatoria"]
+    },
+    Monto:{
+        type:Number,
+        required:[true, "El monto es obligatorio"]
+    },
     Fecha:{
         type:Date,
         required:[true, "el fecha es obligatoria"]
@@ -14,10 +30,18 @@ const PedidoSchema=Schema({
         type:String,
         required:[true, "el factura es obligatorio"]
     },
+    Total:{
+        type: number
+    },
     Estado:{
         type:String,
         enum:['Activo', 'Inactivo'],
         required:[true, 'El estado es obligatorio']
     }
-})
+});
+    PedidoSchema.pre('save', function (next) {
+    
+    this.Total = this.Cantidad * this.Monto ; // Sumar el Subtotal y la cantidad del IVA al Subtotal
+    next();
+});
 module.exports = model('Pedido',PedidoSchema)
